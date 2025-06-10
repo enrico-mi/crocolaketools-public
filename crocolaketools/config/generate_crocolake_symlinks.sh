92c101768fcaced3b4bccac06d8f5f2b33e113c6
Enrico Milanese <enrico.milanese@whoi.edu>
2025-06-10 15:46:56 -0400

    update crocolake links generation for absolute paths

--- crocolaketools/config/generate_crocolake_symlinks.sh
+++ crocolaketools/config/generate_crocolake_symlinks.sh
@@ -8,6 +8,7 @@ set -o pipefail
 # parquet databases that form CrocoLake's data lake.
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+echo "Script directory: $SCRIPT_DIR"
 yaml_file="${SCRIPT_DIR}""/config.yaml"
 crocolake_variants=(
   "PHY"
@@ -29,17 +30,28 @@ for var in "${crocolake_variants[@]}"; do
 
     # Extract the `ln_path` for the CROCOLAKE group
     crocolake_ln=$(yq ".CROCOLAKE_"$var".ln_path" "$yaml_file")
+    echo "ln_path for CROCOLAKE_$var: $crocolake_ln"
     # Check if CROCOLAKE ln_path exists
     if [ -z "$crocolake_ln" ]; then
       echo "CROCOLAKE ln_path not found in the YAML file."
       exit 1
     fi
     crocolake_ln=$(echo "$crocolake_ln" | sed 's/^"//;s/"$//')
-    crocolake_ln=$(realpath "${SCRIPT_DIR}/${crocolake_ln}")
+    if [[ "${crocolake_ln}" != /* ]]; then
+        crocolake_ln=$(realpath "${SCRIPT_DIR}/${crocolake_ln}")
+    else
+        crocolake_ln=$(realpath "${crocolake_ln}")
+    fi
+    echo "ln_path for CROCOLAKE_$var: $crocolake_ln"
 
     crocolake_out=$(yq ".CROCOLAKE_"$var".outdir_pq" "$yaml_file")
     crocolake_out=$(echo "$crocolake_out" | sed 's/^"//;s/"$//')
-    crocolake_out=$(realpath "${SCRIPT_DIR}/${crocolake_out}")
+    if [[ "${crocolake_out}" != /* ]]; then
+        crocolake_out=$(realpath "${SCRIPT_DIR}/${crocolake_out}")
+    else
+        crocolake_out=$(realpath "${crocolake_out}")
+    fi
+    echo "crocolake_out for CROCOLAKE_$var: $crocolake_out"
 
     echo "Creating folder $crocolake_ln if it does not exist"
     mkdir -p $crocolake_ln
@@ -47,15 +59,21 @@ for var in "${crocolake_variants[@]}"; do
     # Extract all `outdir_pq` entries from the YAML file
     yq --arg var "$var" '.[] | select(has("outdir_pq") and .db_type == $var) | .outdir_pq' "$yaml_file" | while read -r outdir_pq; do
       # Skip the CROCOLAKE outdir_pq itself
-      outdir_pq=$(echo "$outdir_pq" | sed 's/^"//;s/"$//')
-      outdir_pq=$(realpath "${SCRIPT_DIR}/${outdir_pq}")
-      echo "Processing $outdir_pq"
+
       # skip if it's the crocolake dir or the other db type (bgc/phy)
-      if [ "$outdir_pq" != "$crocolake_out" ] && [[ "$outdir_pq" != *"ARGO-GDAC"* ]]; then
+      if [[ "$outdir_pq" != *"ARGO-CLOUD"* ]] && [[ "$outdir_pq" != *"ARGO-GDAC"* ]] && [[ "$outdir_pq" != *"CROCOLAKE"* ]]; then
+        echo "Processing outdir_pq: $outdir_pq"
+        outdir_pq=$(echo "$outdir_pq" | sed 's/^"//;s/"$//')
+        if [[ "${outdir_pq}" != /* ]]; then
+          outdir_pq=$(realpath "${SCRIPT_DIR}/${outdir_pq}")
+        else
+          outdir_pq=$(realpath "${outdir_pq}")
+        fi
         # Create a symbolic link in the CROCOLAKE directory
         if [ -d "$outdir_pq" ]; then
-          echo "Creating symlink for $outdir_pq in $crocolake_ln"
-          ln -s $outdir_pq $crocolake_ln
+          db_name=$(basename "$(dirname "$outdir_pq")")
+          echo "Creating symlink for $db_name in $crocolake_ln (points to: $outdir_pq)"
+          ln -s "$outdir_pq" "$crocolake_ln/$db_name"
         else
           echo "Destination directory $outdir_pq does not exist. Skipping symlink creation. This usually happens if you have not generated this parquet dataset first."
         fi
